d5ba664e5b76168367e81536b74aadb684aa0823
Brook <chunyong96@gmail.com>
2023-05-11 01:00:44 +0800

    amend env

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
-chown -R www-data:www-data /var/www/*;
-chown -R 755 /var/www/*;
+# sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+# chown -R www-data:www-data /var/www/*;
+# chown -R 755 /var/www/*;
 touch /run/php/php7.3-fpm.pid;
 
 if [ ! -f /var/www/html/wp-config.php ]; then
