813d32a72bdcea7eda14702f1e87f3dfe5796af4
Brook <chunyong96@gmail.com>
2023-05-10 22:15:43 +0800

    rebuild

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -5,9 +5,8 @@
 # https://www.digicert.com/kb/csr-ssl-installation/nginx-openssl.htm
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -newkey rsa:2048 -nodes -days 365  \
-    -keyout /etc/ssl/chchin.key \
-    -out $CERTS_ \
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
+    -keyout /etc/ssl/nginx.key -out $CERTS_ \
     -subj "/C=MY/ST=Selangor/L=Subang Jaya/O=42/OU=KL/CN=chchin";
 echo "Nginx: ssl is set up!";
 fi
