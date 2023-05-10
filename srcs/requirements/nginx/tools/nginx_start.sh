ef900c873c6e569da0df753c7b06b1886f2992cb
Brook <chunyong96@gmail.com>
2023-05-10 23:13:42 +0800

    Wrong file commited just now

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -3,12 +3,13 @@
 # Creating Your CSR with OpenSSL
 # https://mariadb.com/docs/xpand/security/data-in-transit-encryption/create-self-signed-certificates-keys-openssl/
 # https://www.digicert.com/kb/csr-ssl-installation/nginx-openssl.htm
-if [ ! -f /etc/ssl/certs/nginx.crt ]; then
+if [ ! -f $CERTS_ ]; then
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
-    -keyout /etc/ssl/chchin.key -out $CERTS_ \
+openssl req -x509 -newkey rsa:2048 -nodes -days 365 \
+    -keyout /etc/ssl/chchin.key \
+    -out $CERTS_ \
     -subj "/C=MY/ST=Selangor/L=Subang Jaya/O=42/OU=KL/CN=chchin";
-echo "Nginx: ssl is set up!";
+echo "Nginx: ssl set up successful!";
 fi
 
 exec "$@"
