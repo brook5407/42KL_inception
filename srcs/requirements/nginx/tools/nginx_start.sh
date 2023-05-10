489b04c931ceac8eab55653380f518dc85612f57
Brook <chunyong96@gmail.com>
2023-05-11 00:11:36 +0800

    debug ssl

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -3,11 +3,10 @@
 # Creating Your CSR with OpenSSL
 # https://mariadb.com/docs/xpand/security/data-in-transit-encryption/create-self-signed-certificates-keys-openssl/
 # https://www.digicert.com/kb/csr-ssl-installation/nginx-openssl.htm
+if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -newkey rsa:2048 -nodes -days 365 \
-    -keyout /etc/ssl/chchin.key \
-    -out $CERTS_ \
-    -subj "/C=MY/ST=Selangor/L=Subang Jaya/O=42/OU=KL/CN=chchin";
-echo "Nginx: ssl set up successful!";
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=wordpress/CN=crendeha.42.fr";
+echo "Nginx: ssl is set up!";
+fi
 
 exec "$@"
